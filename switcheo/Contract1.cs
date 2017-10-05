2206cc71b81368bdd4e944907eb88782032ffac5
Ivan Poon <ravenxce@gmail.com>
2017-10-05 22:54:33 +0800

    Add project files.

---
+++ switcheo/Contract1.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace switcheo
+{
+    public class Contract1 : Framework.SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
