5c037f97b70f84e01684a82148ece8c6af7e9a31
switcheo <henry.chua@switcheo.network>
2019-02-19 16:01:10 +0800

    Use Opcode isntead to append array

--- switcheo/BalanceEngine.cs
+++ switcheo/BalanceEngine.cs
@@ -1,66 +1,55 @@
 
 using Neo.SmartContract.Framework;
+using Neo.VM;
 using System;
 using System.Numerics;
 
 namespace switcheo
 {
-  public struct BalanceChange
-  {
-    public byte[] AssetID;
-    public BigInteger Amount;
-    public byte[] ReasonCode;
-  }
-  public static class BalanceEngineHelpers
-  {
-
-    public static Map<byte[], BalanceChange[]> IncreaseBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
+    public struct BalanceChange
     {
-      return ChangeBalance(balanceChanges, address, assetID, amount, reason);
+        public byte[] AssetID;
+        public BigInteger Amount;
+        public byte[] ReasonCode;
     }
 
-    public static Map<byte[], BalanceChange[]> ReduceBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
+    public static class BalanceEngineHelpers
     {
-      return ChangeBalance(balanceChanges, address, assetID, 0 - amount, reason);
-    }
+        [OpCode(OpCode.APPEND)]
+        public extern static void Append(BalanceChange[] array, BalanceChange item);
 
-    private static Map<byte[], BalanceChange[]> ChangeBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
-    {
-      if (amount < 1) throw new ArgumentOutOfRangeException();
-      BalanceChange balanceChange = new BalanceChange
-      {
-        AssetID = assetID,
-        Amount = 0 + amount,
-        ReasonCode = reason
-      };
-      // create new list if its a new address
-      if (!balanceChanges.HasKey(address))
-      {
-        balanceChanges[address] = new BalanceChange[] { balanceChange };
-      }
-      else
-      {
-        balanceChanges[address] = AddToArray(balanceChanges[address], balanceChange);
-      }
-      return balanceChanges;
-    }
+        public static Map<byte[], BalanceChange[]> IncreaseBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
+        {
+            return ChangeBalance(balanceChanges, address, assetID, amount, reason);
+        }
 
-    private static T[] AddToArray<T>(this T[] target, params T[] items)
-    {
-      if (target == null)
-      {
-        target = new T[] { };
-      }
-      if (items == null)
-      {
-        items = new T[] { };
-      }
+        public static Map<byte[], BalanceChange[]> ReduceBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
+        {
+            return ChangeBalance(balanceChanges, address, assetID, 0 - amount, reason);
+        }
 
-      // Join the arrays
-      T[] result = new T[target.Length + items.Length];
-      target.CopyTo(result, 0);
-      items.CopyTo(result, target.Length);
-      return result;
+        private static Map<byte[], BalanceChange[]> ChangeBalance(this Map<byte[], BalanceChange[]> balanceChanges, byte[] address, byte[] assetID, BigInteger amount, byte[] reason)
+        {
+            BalanceChange balanceChange = new BalanceChange
+            {
+                AssetID = assetID,
+                Amount = amount,
+                ReasonCode = reason
+            };
+            if (balanceChanges.HasKey(address))
+            {
+                var changes = balanceChanges[address];
+
+                Append(changes, balanceChange);
+
+                balanceChanges[address] = changes;
+            }
+            else
+            {
+                // create new array if its a new address
+                balanceChanges[address] = new BalanceChange[] { balanceChange };
+            }
+            return balanceChanges;
+        }
     }
-  }
 }
